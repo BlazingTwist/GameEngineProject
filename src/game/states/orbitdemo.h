153a6e78d78fabf0439f6563c6a07a580ee0b1f0
BlazingTwist <39350649+BlazingTwist@users.noreply.github.com>
2021-12-01 09:10:08 +0100

    implement orbitDemo

--- src/game/states/orbitdemo.h
+++ src/game/states/orbitdemo.h
@@ -2,6 +2,16 @@
 #define ACAENGINE_ORBITDEMO_H
 
 #include <engine/gamestate/basegamestate.h>
+#include <engine/graphics/lightdata.h>
+#include <engine/graphics/camera.hpp>
+#include <engine/graphics/core/sampler.hpp>
+#include <engine/graphics/renderer/mesh.hpp>
+#include <engine/graphics/renderer/meshrenderer.hpp>
+#include <engine/graphics/resources.hpp>
+#include <engine/input/inputmanager.hpp>
+#include <engine/gamestate/gamestatemanager.h>
+#include "mainstate.h"
+#include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
 
 namespace gameState {
@@ -17,6 +27,53 @@ namespace gameState {
         void onResume() override;
 
         void onPause() override;
+
+    private:
+        glm::vec3 ambientLightData;
+        graphics::LightData lightData;
+        graphics::Camera camera;
+        graphics::MeshRenderer meshRenderer;
+
+        graphics::Mesh sphereMesh;
+        utils::MeshData *sphereInvertedMeshData;
+        graphics::Mesh *sphereInvertedMesh = nullptr;
+
+        unsigned int planetID = 0;
+        unsigned int sunID = 0;
+
+        graphics::Program program = graphics::Program();
+        GLint worldToCameraMatrixID = 0;
+        GLint cameraPositionShaderID = 0;
+        GLint glsl_ambient_light = 0;
+
+        bool hotkey_reset_isDown = false;
+        bool hotkey_mainState_isDown = false;
+        bool hotkey_exit_isDown = false;
+        glm::vec2 prevCursorPos = glm::vec2(0.0f, 0.0f);
+        float cameraPitch = 0.0f;
+        float cameraYaw = 0.0f;
+        float cameraRoll = 0.0f;
+
+        glm::vec3 planetPosition = {0.0f, 0.0f, 0.0f};
+        glm::vec3 planetVelocity = {0.0f, 0.0f, 0.0f};
+        glm::vec3 sunPosition = {0.0f, 0.0f, 0.0f};
+        glm::vec3 sunVelocity = {0.0f, 0.0f, 0.0f};
+
+        void initializeControls();
+
+        void initializeCursorPosition();
+
+        void initializeShaders();
+
+        void loadShaders();
+
+        void loadGeometry();
+
+        void initializeScene();
+
+        void bindLighting();
+
+        void bindCamera();
 
     };
 }
