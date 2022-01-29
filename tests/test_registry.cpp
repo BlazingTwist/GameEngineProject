d5184f06a88eeeb0d20854008fb6add65df62d91
BlazingTwist <39350649+BlazingTwist@users.noreply.github.com>
2022-01-29 17:25:00 +0100

    adjust test_registry.cpp to match Entity-Registry Implementation

--- tests/test_registry.cpp
+++ tests/test_registry.cpp
@@ -1,7 +1,5 @@
 #include "testutils.hpp"
 
-#pragma error include registry file!!
-
 #include "engine/entity/entityregistry.h"
 #include <optional>
 #include <vector>
@@ -18,92 +16,96 @@ int main() {
 
     entity::EntityRegistry &registry = entity::EntityRegistry::getInstance();
 
-    // TODO make sure you delete these pointers
     std::vector<entity::EntityReference *> entities;
 
+    entities.reserve(5);
     for (int i = 0; i < 5; ++i) {
         entities.push_back(registry.createEntity());
     }
 
-    auto refDel = registry.getRef(entities[2]);
+    auto refDel = entities[2];
 
-    EXPECT(registry.getEntity(refDel), "Reference is valid after creation.");
-    registry.erase(entities[2]);
-    EXPECT(!registry.getEntity(refDel), "Reference is invalid after delete.");
+    EXPECT(registry.getEntityData(refDel), "Reference is valid after creation.");
+    registry.eraseEntity(refDel);
+    EXPECT(!registry.getEntityData(refDel), "Reference is invalid after delete.");
 
-    entities[2] = registry.create();
+    entities[2] = registry.createEntity();
     for (int i = 0; i < 6; ++i)
-        entities.push_back(registry.create());
+        entities.push_back(registry.createEntity());
 
-    EXPECT(!registry.getEntity(refDel), "Reference remains invalid after reuse of the id.");
+    EXPECT(!registry.getEntityData(refDel), "Reference remains invalid after reuse of the id.");
+
+    delete refDel; // done playing with this.
 
     {
-        ComponentAccess <Foo> fooComps = registry.getComponents<Foo>();
         for (int i = 0; i < static_cast<int>(entities.size()); ++i) {
-            const Foo &foo = fooComps.insert(entities[i], Foo{i});
-            EXPECT(foo.i == i, "Add a component.");
+            registry.addOrSetComponent<Foo>(entities[i], Foo{i});
+            std::optional<const Foo> foo = registry.getComponentData<Foo>(entities[i]);
+            EXPECT(foo.has_value() && foo->i == i, "Add a component.");
         }
     }
 
     {
-        ComponentAccess <Bar> barComps = registry.getComponents<Bar>();
         for (int i = 0; i < static_cast<int>(entities.size()); i += 3) {
-            const Bar &bar = barComps.insert(entities[i], Bar{static_cast<float>(i)});
-            EXPECT(bar.f == static_cast<float>(i), "Add a component.");
+            registry.addOrSetComponent<Bar>(entities[i], Bar{static_cast<float>(i)});
+            std::optional<const Bar> bar = registry.getComponentData<Bar>(entities[i]);
+            EXPECT(bar.has_value() && bar->f == static_cast<float>(i), "Add a component.");
         }
     }
 
     {
-        ComponentAccess <Foo> fooComps = registry.getComponents<Foo>();
-        const ComponentAccess <Foo> constFooComps = fooComps;
-        for (int i = 0; i < static_cast<int>(entities.size()); ++i) {
-            EXPECT(fooComps.at(entities[i]), "Retrieve a component.");
-            EXPECT(constFooComps.at(entities[i]), "Retrieve a component.");
+
+        for (auto &entity: entities) {
+            EXPECT(registry.getComponentData<Foo>(entity), "Retrieve a component.");
         }
 
-        fooComps.erase(entities[0]);
-        fooComps.erase(entities[1]);
+        registry.removeComponent<Foo>(entities[0]);
+        registry.removeComponent<Foo>(entities[1]);
 
-        EXPECT(!fooComps.at(entities[0]), "Remove a component.");
-        EXPECT(!fooComps.at(entities[1]), "Remove a component.");
+        EXPECT(!registry.getComponentData<Foo>(entities[0]), "Remove a component.");
+        EXPECT(!registry.getComponentData<Foo>(entities[1]), "Remove a component.");
 
 
-        registry.erase(entities[2]);
+        registry.eraseEntity(entities[2]);
 
         for (int i = 3; i < static_cast<int>(entities.size()); ++i) {
-            EXPECT(fooComps.at(entities[i]), "Other components are untouched.");
-            EXPECT(fooComps.at(entities[i])->i == i, "Other components are untouched.");
+            EXPECT(registry.getComponentData<Foo>(entities[i]), "Other components are untouched.");
+            EXPECT(registry.getComponentData<Foo>(entities[i])->i == i, "Other components are untouched.");
         }
     }
 
     int sum = 0;
-    registry.execute([&sum](const Foo &foo) { sum += foo.i; });
+    registry.execute([&sum](const Foo foo) { sum += foo.i; });
     // without auto deduction
     // registry.execute<Foo>([&sum](const Foo& foo) { sum += foo.i; });
     EXPECT(sum == 10 * 11 / 2 - 3, "Execute action on a single component type.");
 
     sum = 0;
-    registry.execute([&sum](const Bar &bar, const Foo &foo) { sum += foo.i - 2 * static_cast<int>(bar.f); });
+    registry.execute([&sum](const Bar bar, const Foo foo) { sum += foo.i - 2 * static_cast<int>(bar.f); });
     //registry.execute<Bar,Foo>([&sum](const Bar& bar, const Foo& foo) { sum += foo.i - 2 * static_cast<int>(bar.f); });
     EXPECT(sum == -3 - 6 - 9, "Execute action on multiple component types.");
 
     // registry.execute<Entity, Bar>([&](Entity ent, Bar& bar)
     {
-        auto barComps = registry.getComponents<Bar>();
-        registry.execute([&](Entity ent, Bar &bar) {
-            auto pBar = barComps.at(ent);
+        registry.execute([&](const entity::EntityReference *ent, const Bar bar) {
+            auto pBar = registry.getComponentData<Bar>(ent);
             EXPECT(pBar, "Execute provides the correct entity.");
             EXPECT(pBar->f == bar.f, "Execute provides the correct entity.");
-            bar.f = -1.f;
+            registry.addOrSetComponent(ent, Bar{-1.f}); // TODO I'm have mixed feelings about this.
         });
     }
 
     {
-        const ComponentAccess <Bar> barComps = registry.getComponents<Bar>();
         for (size_t i = 3; i < entities.size(); i += 3) {
-            auto pBar = barComps.at(entities[i]);
+            auto pBar = registry.getComponentData<Bar>(entities[i]);
             EXPECT(pBar, "Action can change components.");
             EXPECT(pBar->f == -1.f, "Action can change components.");
         }
     }
+
+    for (entity::EntityReference *entity: entities) {
+        registry.eraseEntity(entity);
+        delete entity;
+    }
+    entities.clear();
 }
