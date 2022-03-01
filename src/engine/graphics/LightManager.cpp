636c60a5a8c75da0e8b8bd4aa63e956f92af5a84
BlazingTwist <39350649+BlazingTwist@users.noreply.github.com>
2022-03-01 11:39:59 +0100

    add Singleton "LightManager" to handle pushing Light components into the SSBO

---
+++ src/engine/graphics/LightManager.cpp
@@ -0,0 +1,72 @@
+﻿#include "LightManager.h"
+
+namespace graphics {
+
+    void LightManager::LightSystem::execute() {
+        bool boundLightCountChanged = false;
+        std::vector<const entity::EntityReference *> changedLights = {};
+        // make sure all Lights are registered in LightManager
+        registry.execute([this, &boundLightCountChanged, &changedLights](const entity::EntityReference *entity, components::Light light) {
+            if (light.getLightManagerId() < 0) {
+                light.setLightManagerId(static_cast<int>(lightManager.boundLightCount));
+                lightManager.boundLightCount++;
+                boundLightCountChanged = true;
+                lightManager.boundLights.push_back(entity);
+                registry.addOrSetComponent(entity, light);
+            }
+            if (light.isLightDataChanged()) {
+                changedLights.push_back(entity);
+            }
+        });
+
+        bool boundLightsChanged = !changedLights.empty();
+        if (boundLightCountChanged || boundLightsChanged) {
+            glCall(glBindBuffer, GL_SHADER_STORAGE_BUFFER, lightManager.lightSSBO);
+            bool bufferWasResized = false;
+            if (boundLightCountChanged) {
+                if (lightManager.boundLightCount > lightManager.currentPossibleBoundLightCount) {
+                    bufferWasResized = true;
+                    lightManager.performResize();
+                }
+                glCall(glBufferSubData, GL_SHADER_STORAGE_BUFFER, 0, sizeof(unsigned int), &lightManager.boundLightCount);
+            }
+            if (!bufferWasResized && boundLightsChanged) {
+                for (const entity::EntityReference *changedEntity: changedLights) {
+                    components::Light changedLight = registry.getComponentData<components::Light>(changedEntity).value();
+                    glCall(glBufferSubData, GL_SHADER_STORAGE_BUFFER,
+                           LightCountField_ByteOffset + (changedLight.getLightManagerId() * sizeof(graphics::LightData)),
+                           sizeof(graphics::LightData), &changedLight.getLightData());
+                    changedLight.lightDataChangeHandled();
+                    registry.addOrSetComponent(changedEntity, changedLight);
+                }
+            }
+        }
+    }
+
+    void LightManager::removeLight(const entity::EntityReference *lightEntity) {
+        removeLight(entity::EntityRegistry::getInstance().getComponentData<components::Light>(lightEntity).value());
+    }
+    
+    void LightManager::removeLight(const components::Light &light) {
+        if (boundLightCount == 0) {
+            spdlog::error("tried to remove light, but LightManager is empty!");
+            return;
+        }
+
+        boundLightCount--;
+        glCall(glBindBuffer, GL_SHADER_STORAGE_BUFFER, lightSSBO);
+        glCall(glBufferSubData, GL_SHADER_STORAGE_BUFFER, 0, sizeof(unsigned int), &boundLightCount);
+        if (light.getLightManagerId() != boundLightCount) {
+            const entity::EntityReference *moveEntity = boundLights.back();
+            boundLights[light.getLightManagerId()] = moveEntity;
+
+            components::Light movedLight = entity::EntityRegistry::getInstance().getComponentData<components::Light>(moveEntity).value();
+            movedLight.setLightManagerId(light.getLightManagerId());
+            entity::EntityRegistry::getInstance().addOrSetComponent(moveEntity, movedLight);
+            glCall(glBufferSubData, GL_SHADER_STORAGE_BUFFER, LightCountField_ByteOffset + (movedLight.getLightManagerId() * sizeof(graphics::LightData)),
+                   sizeof(graphics::LightData), &movedLight.getLightData());
+        }
+        boundLights.pop_back();
+    }
+
+}
---
+++ src/engine/graphics/LightManager.h
@@ -0,0 +1,92 @@
+﻿#ifndef ACAENGINE_LIGHTMANAGER_H
+#define ACAENGINE_LIGHTMANAGER_H
+
+#include <vector>
+#include <engine/components/light.h>
+#include <engine/entity/entityregistry.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <GL/glew.h>
+#include "core/opengl.hpp"
+
+namespace graphics {
+    static constexpr int LightCountField_ByteOffset = 16; // don't ask why, I'm not sure either, has something to do with OpenGLs memory alignment rules.
+
+    class LightManager {
+    public:
+        class LightSystem {
+        public:
+            explicit LightSystem(entity::EntityRegistry &_registry) : registry(_registry), lightManager(LightManager::getInstance()) {}
+
+            void execute();
+
+        private:
+            entity::EntityRegistry &registry;
+            LightManager &lightManager;
+        };
+
+
+        static LightManager &getInstance() {
+            static LightManager instance;
+            return instance;
+        }
+
+        void removeLight(const entity::EntityReference *light);
+        
+        void removeLight(const components::Light &light);
+
+        void bindLights(int bindIndex) const {
+            glCall(glBindBuffer, GL_SHADER_STORAGE_BUFFER, lightSSBO);
+            glCall(glBindBufferBase, GL_SHADER_STORAGE_BUFFER, bindIndex, lightSSBO);
+        }
+
+        LightManager(LightManager const &) = delete;
+
+        void operator=(LightManager const &) = delete;
+
+        virtual ~LightManager() {
+            for (auto *item: boundLights) {
+                delete item;
+            }
+            boundLights.clear();
+        }
+
+    private:
+        LightManager() : lightSSBO(0) {
+            glCall(glGenBuffers, 1, &lightSSBO);
+            glCall(glBindBuffer, GL_SHADER_STORAGE_BUFFER, lightSSBO);
+            //glCall(glBufferData, GL_SHADER_STORAGE_BUFFER, sizeof(unsigned int) + sizeof(graphics::LightData), nullptr, GL_DYNAMIC_DRAW);
+            glCall(glBufferData, GL_SHADER_STORAGE_BUFFER, LightCountField_ByteOffset, nullptr, GL_DYNAMIC_DRAW);
+            glCall(glBufferSubData, GL_SHADER_STORAGE_BUFFER, 0, sizeof(unsigned int), &boundLightCount);
+        };
+
+        void performResize() {
+            currentPossibleBoundLightCount = static_cast<unsigned int>(static_cast<float>(boundLightCount + 1) * 1.3f);
+            const unsigned int lightDataByteSize = currentPossibleBoundLightCount * sizeof(graphics::LightData);
+            glCall(glBufferData, GL_SHADER_STORAGE_BUFFER,
+                   LightCountField_ByteOffset + lightDataByteSize,
+                   nullptr, GL_DYNAMIC_DRAW);
+
+            auto *lights = new LightData[boundLightCount];
+            entity::EntityRegistry &registry = entity::EntityRegistry::getInstance();
+            for (unsigned int i = 0; i < boundLightCount; i++) {
+                components::Light lightComp = registry.getComponentData<components::Light>(boundLights[i]).value();
+                lights[i] = lightComp.getLightData();
+                if (lightComp.isLightDataChanged()) {
+                    lightComp.lightDataChangeHandled();
+                    registry.addOrSetComponent(boundLights[i], lightComp);
+                }
+            }
+            glCall(glBufferSubData, GL_SHADER_STORAGE_BUFFER, LightCountField_ByteOffset, static_cast<int>(lightDataByteSize), lights);
+            delete[] lights;
+        }
+
+        GLuint lightSSBO;
+        unsigned int boundLightCount = 0;
+        unsigned int currentPossibleBoundLightCount = 0;
+        std::vector<const entity::EntityReference *> boundLights = {};
+
+    };
+}
+
+#endif //ACAENGINE_LIGHTMANAGER_H
